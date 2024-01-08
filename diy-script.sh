cfe7dbf3bc0545e7b5f30681d6cc6161acba030e
dd0072 <52378245+dd0072@users.noreply.github.com>
2024-01-08 22:11:50 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -40,7 +40,7 @@ git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # svn export https://github.com/haiibo/packages/trunk/luci-app-vssr package/luci-app-vssr
 # git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
 # svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
 # svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
