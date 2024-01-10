977d12987a882543ae0361e53d1e40ed41fd202b
dd0072 <52378245+dd0072@users.noreply.github.com>
2024-01-10 11:38:12 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -5,7 +5,7 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 
 # 添加插件包
 git clone https://github.com/dd0072/openwrt-packages.git
-git clone https://github.com/dd0072/small.git
+git clone https://github.com/dd0072/small-package.git
 
 # 删除冲突的插件
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
