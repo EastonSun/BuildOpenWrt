e68a57c16503f5e6860c097f117a4e58a5c496b0
dd0072 <52378245+dd0072@users.noreply.github.com>
2024-01-13 10:07:52 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -4,8 +4,22 @@
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # 添加插件包
-git clone https://github.com/kenzok8/openwrt-packages.git
-git clone https://github.com/kenzok8/small.git
+# merge_package() {
+	# 参数1是分支名, 参数2是子目录, 参数3是目标目录, 参数4仓库地址
+	# trap 'rm -rf "$TMP_DIR"' 0 1 2 3
+	# TMP_DIR="$(mktemp -d)" || exit 1
+ # ORI_DIR="$PWD"
+	# [ -d "$3" ] || mkdir -p "$3"
+	# TGT_DIR="$(cd "$3"; pwd)"
+	# cd "$TMP_DIR" && \
+	# git init >/dev/null 2>&1 && \
+	# git remote add -f origin "$4" >/dev/null 2>&1 && \
+	# git checkout "remotes/origin/$1" -- "$2" && \
+	# cd "$2" && cp -a . "$TGT_DIR/" && cd "$ORI_DIR"
+# }
+# merge_package "master" "luci-app-openclash" "package/openwrt-packages/luci-app-openclash" "https://github.com/WYC-2020/openwrt-packages"
+git clone --depth=1 -b master https://github.com/kenzok8/openwrt-packages.git
+git clone --depth=1 -b master https://github.com/kenzok8/small.git
 
 # 删除冲突的插件
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
