c5da22cbe9369c7e77d3122ccfe890230575d8e4
dd0072 <52378245+dd0072@users.noreply.github.com>
2024-02-13 20:36:57 +0800

    Update OpenWrt-x86_64.sh

--- scripts/OpenWrt-x86_64.sh
+++ scripts/OpenWrt-x86_64.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
@@ -10,19 +13,31 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# svn_export "master" "target/linux/x86" "route" "https://github.com/coolsnowwolf/lede"
+# git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 
 # 添加netdata
-git_sparse_clone master https://github.com/immortalwrt/packages/trunk/admin netdata
+git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+# 添加adguardhome
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 
-git clone --depth=1 -b master https://github.com/kenzok8/openwrt-packages package/openwrt-package
-git clone --depth=1 -b master https://github.com/kenzok8/small package/small
+# 添加passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+
+# 添加opcnclash
+# git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # 添加Turboacc
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
-# ./scripts/feeds update -a
-# ./scripts/feeds install -a
+# 添加MosDNS
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# git clone --depth=1 -b master https://github.com/kenzok8/openwrt-packages package/openwrt-package
+# git clone --depth=1 -b master https://github.com/kenzok8/small package/small
+
+./scripts/feeds update -a
+./scripts/feeds install -a
