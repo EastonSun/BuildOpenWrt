d95166bbaac0cd987e453a22ea66c756b349670d
dd0072 <52378245+dd0072@users.noreply.github.com>
2024-02-13 14:31:11 +0800

    Update OpenWrt-x86_64.sh

--- scripts/OpenWrt-x86_64.sh
+++ scripts/OpenWrt-x86_64.sh
@@ -19,9 +19,16 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 # }
 # merge_package "master" "luci-app-openclash" "package/openwrt-packages/luci-app-openclash" "https://github.com/WYC-2020/openwrt-packages"
 
-git clone --depth=1 -b master https://github.com/kenzok8/openwrt-packages package/openwrt-package
-git clone --depth=1 -b master https://github.com/kenzok8/small package/small
+# git clone --depth=1 -b master https://github.com/kenzok8/openwrt-packages package/openwrt-package
+# git clone --depth=1 -b master https://github.com/kenzok8/small package/small
 
+# 添加MosDNS
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# 添加passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
