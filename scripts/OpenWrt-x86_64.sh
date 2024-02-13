372516650ef13bf0259a24a45832c0bd19167448
dd0072 <52378245+dd0072@users.noreply.github.com>
2024-02-13 20:06:18 +0800

    Update OpenWrt-x86_64.sh

--- scripts/OpenWrt-x86_64.sh
+++ scripts/OpenWrt-x86_64.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-svn_export() {
-	# 参数1是分支名, 参数2是子目录, 参数3是目标目录, 参数4仓库地址
-	trap 'rm -rf "$TMP_DIR"' 0 1 2 3
-	TMP_DIR="$(mktemp -d)" || exit 1
-	[ -d "$3" ] || mkdir -p "$3"
-	TGT_DIR="$(cd "$3"; pwd)"
-	cd "$TMP_DIR" && \
-	git init >/dev/null 2>&1 && \
-	git remote add -f origin "$4" >/dev/null 2>&1 && \
-	git checkout "remotes/origin/$1" -- "$2" && \
-	cd "$2" && cp -a . "$TGT_DIR/"
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
 }
 
 # svn_export "master" "target/linux/x86" "route" "https://github.com/coolsnowwolf/lede"
 
 # 添加netdata
-svn_export "master" "admin/netdata" "package" "https://github.com/immortalwrt/packages"
+git_sparse_clone master https://github.com/immortalwrt/packages/trunk/admin netdata
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
