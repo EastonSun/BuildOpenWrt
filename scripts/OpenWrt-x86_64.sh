de03d5774a6896a0a2bcb7761bc04e3a06461dac
dd0072 <52378245+dd0072@users.noreply.github.com>
2024-03-08 22:39:00 +0800

    Update OpenWrt-x86_64.sh

--- scripts/OpenWrt-x86_64.sh
+++ scripts/OpenWrt-x86_64.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
-
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
