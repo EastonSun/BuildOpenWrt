59b1788aa709526b73d2d92709b40d4c901753f9
dd0072 <52378245+dd0072@users.noreply.github.com>
2024-04-24 20:29:12 +0800

    添加ddns-go

--- scripts/OpenWrt-x86_64.sh
+++ scripts/OpenWrt-x86_64.sh
@@ -18,6 +18,9 @@ function git_sparse_clone() {
 # 添加adguardhome
 # git_sparse_clone master https://github.com/kiddin9/openwrt-packages adguardhome luci-app-adguardhome
 
+# 添加ddns-go
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages ddns-go luci-app-ddns-go
+
 # 删除不需要的文件
 # rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 rm -rf feeds/packages/lang/golang
