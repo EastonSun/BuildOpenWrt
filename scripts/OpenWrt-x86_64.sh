68ada76a16cf47cfd532a56be15b3b6fff12f010
dd0072 <52378245+dd0072@users.noreply.github.com>
2024-04-28 22:54:23 +0800

    去除MOSDNS

--- scripts/OpenWrt-x86_64.sh
+++ scripts/OpenWrt-x86_64.sh
@@ -46,8 +46,8 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-pass
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # 添加MosDNS
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # 替换golang版本为1.22.x
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
